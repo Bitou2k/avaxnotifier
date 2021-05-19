601ea01e30e523fc8d5f01ba371258a87eed6433
Dmytro Savchenko <login.was.here@gmail.com>
2021-05-19 22:41:10 +0300

    Add showing address chain in transaction message

--- src/util/messages/transaction.js
+++ src/util/messages/transaction.js
@@ -2,6 +2,22 @@ const maxTransactionsToShowDefault = 10
 
 const getShortAddress = (address) => address ? `${address.substring(0, 6)}...${address.substring(address.length - 4)}` : ''
 
+const prepareChainID = (chainID) => {
+  if (chainID === '11111111111111111111111111111111LpoYY') {
+    return 'P-'
+  }
+  if (chainID === '2oYMBNV4eNHyqk2fjjV5nVQLDbtmNJzq5s3qs3Lo6ftnC6FByM') {
+    return 'X-'
+  }
+  if (chainID === '2q9e4r6Mu3U68nU1fYjgbR6JvwrRx36CohpAX5UQxse55x1Q5') {
+    return 'C-'
+  }
+  return ''
+}
+
+const prepareAddress = (address, chainID) => `${prepareChainID(chainID)}${address}`
+const prepareShortAddress = (address, chainID) => `${prepareChainID(chainID)}${getShortAddress(address)}`
+
 function compareNumbers(a, b) {
   return (b.amount / 1000000000) - (a.amount / 1000000000);
 }
@@ -16,7 +32,7 @@ ${data.transaction.inputs
   .slice()
   .sort(compareNumbers)
   .map(input => {
-    return `${data.addresses.includes(input.address) ? '*' : ''}💰 ${(input.amount / 1000000000).toFixed(2)} ${data.assets[input.assetID]}${data.addresses.includes(input.address) ? '*' : ''} from [${getShortAddress(input.address)}](https://explorer.avax.network/address/${input.address})`
+    return `${data.addresses.includes(input.address) ? '*' : ''}💰 ${(input.amount / 1000000000).toFixed(2)} ${data.assets[input.assetID]}${data.addresses.includes(input.address) ? '*' : ''} from [${prepareShortAddress(input.address, input.chainID)}](https://explorer.avax.network/address/${prepareAddress(input.address, input.chainID)})`
   })
   .join('\n')}
 ` : ''}
@@ -26,7 +42,7 @@ ${data.transaction.outputs
   .slice()
   .sort(compareNumbers)
   .map(output => {
-    return `${data.addresses.includes(output.address) ? '*' : ''}💰 ${(output.amount / 1000000000).toFixed(2)} ${data.assets[output.assetID]}${data.addresses.includes(output.address) ? '*' : ''} to [${getShortAddress(output.address)}](https://explorer.avax.network/address/${output.address})`
+    return `${data.addresses.includes(output.address) ? '*' : ''}💰 ${(output.amount / 1000000000).toFixed(2)} ${data.assets[output.assetID]}${data.addresses.includes(output.address) ? '*' : ''} to [${prepareShortAddress(output.address, output.chainID)}](https://explorer.avax.network/address/${prepareAddress(output.address, output.chainID)})`
   })
   .join('\n')}
 ` : ''}
